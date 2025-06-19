1c259cc924b520d2f36812f770acdca898746903
Aditya Raj <adityaraj520@gmail.com>
2025-06-19 14:54:31 +0530

    Cleanup: Remove obsolete WebConfig and all @CrossOrigin annotations. All CORS is now handled in SecurityConfig only.

--- backend/src/main/java/com/codewithaditya/blog/controller/BlogPostController.java
+++ backend/src/main/java/com/codewithaditya/blog/controller/BlogPostController.java
@@ -15,7 +15,6 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/posts")
-@CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 public class BlogPostController {
     private final BlogPostService blogPostService;
